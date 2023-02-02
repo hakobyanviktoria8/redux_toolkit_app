053946bc0a7d0bbd5e5fc6faf8a63aee05306128
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-02 23:17:37 +0400

    check all tests

--- src/features/counter/counterSlice.spec.js
+++ src/features/counter/counterSlice.spec.js
@@ -5,28 +5,20 @@ describe("counter reducer", () => {
     value: 3,
     status: "idle",
   };
+  const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
   it("should handle increment", () => {
-    const actual = counterSlice.reducer(
-      initialState,
-      counterSlice.actions.increment()
-    );
+    const actual = counterSlice.reducer(initialState, increment());
     expect(actual.value).toEqual(4);
   });
 
   it("should handle decrement", () => {
-    const actual = counterSlice.reducer(
-      initialState,
-      counterSlice.actions.decrement()
-    );
+    const actual = counterSlice.reducer(initialState, decrement());
     expect(actual.value).toEqual(2);
   });
 
   it("should handle incrementByAmount", () => {
-    const actual = counterSlice.reducer(
-      initialState,
-      counterSlice.actions.incrementByAmount(2)
-    );
+    const actual = counterSlice.reducer(initialState, incrementByAmount(2));
     expect(actual.value).toEqual(5);
   });
 });
